f3992b0b45385489b2146b95432fa0d8cdced9be
JEONGMIN RHEE <117976397+LEEJEONGMIN07@users.noreply.github.com>
2022-11-25 13:55:22 +0900

    3
    3

--- sketch.js
+++ sketch.js
@@ -1,17 +1,34 @@
-let musicfile;
+var musicfile;
+var button;
 
 function setup()
 {
     soundFormats('mp3','ogg');
-    musicfile = loadSound('christmas', loadMusic);
-    //musicfile.play();
+    musicfile = loadSound('christmas', loaded);
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
+    background(50);
 
 }
 
-function loadMusic()
+function togglePlaying()
 {
-    musicfile.play(); //
+    if(!musicfile.isPlaying()){
+        musicfile.play();
+        musicfile.setVolume(0.3);
+        button.html('pause');
+    }
+
+    else{musicfile.stop();
+    button.html('play');}
+
 }
 
-function draw()
-{}
+function loaded()
+{
+    musicfile.play();
+    console.log('loaded');
+}
+
+
+
